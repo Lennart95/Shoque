4da4bafcbd48bf6072919d7c14c2597567dcf789
Wesley Oosterveen <wesleyoosterveen94@gmail.com>
2014-02-17 13:00:36 +0100

    Activity toegevoegt

--- gen/com/example/shoque/R.java
+++ gen/com/example/shoque/R.java
@@ -28,14 +28,17 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
+        public static final int activity_wesley=0x7f030001;
     }
     public static final class menu {
         public static final int home=0x7f070000;
+        public static final int wesley=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int title_activity_wesley=0x7f050003;
     }
     public static final class style {
         /** 
--- src/com/example/shoque/Wesley.java
+++ src/com/example/shoque/Wesley.java
@@ -4,20 +4,19 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Home extends Activity {
+public class Wesley extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(R.layout.activity_wesley);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.home, menu);
+		getMenuInflater().inflate(R.menu.wesley, menu);
 		return true;
 	}
 
 }
-// very nice :D - Wesley 2014
