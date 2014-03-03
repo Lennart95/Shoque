e2176b9d2b7f69cec92620d2d3fe0222d6c9b068
cmitz <cmitzz@gmail.com>
2014-03-03 16:08:34 +0100

    SimpleAI, very, very simple

--- src/com/example/shoque/SimpleAI.java
+++ src/com/example/shoque/SimpleAI.java
@@ -12,6 +12,21 @@ public class SimpleAI {
 		this.game = game;
 	}
 	
+	/**
+	 * Set the ships: result [[index],[ship,direction]]
+	 * Ship:					Direction:
+	 * Flightdeckship = 0		From origin west = 0
+	 * Battleship = 1			From origin south = 1
+	 * Submarine = 2
+	 * Torpedohunter = 3
+	 * Minesweeper = 4
+	 */
+	public int[][] setShips(){
+		int[][] result = {{0,0,1},{1,1,1},{2,2,1},{3,3,1},{4,4,1}};
+		
+		
+		return result;
+	}
 	
 	/**
 	 * When getMove() is called, return an random field that has not yet been hit.
@@ -19,20 +34,24 @@ public class SimpleAI {
 	 */
 	public int[] getMove(){
 		int i;
-		int[] move = {-1, -1};
+		
 		boolean legal = false;
 		
-		while (!legal) {
-			i = 1 + (int)Math.random()*100;
-			if (game.getBoard().isEmpty(i)){
-				legal = true;
+		// Board must not be full (so there are no empty spots) 
+		// and the loop must stop as soon as it has found a legal spot
+		while (!legal && !game.isFull()) {
+			i = (int)Math.random()*99;			//Generate Random between 0 and 99
+			if (game.getBoard().isEmpty(i)){	//Check if random is emtpy
+				legal = true;					//If so, break the loop and submit empty spot
 			}
 		}
 		
-		int x;
-		int y;
-		y = i % game.DIM;
+		int x = 0;
+			x = i % game.DIM;
+		int y = 0;
+			y = i / game.DIM;
 		
+		int[] move = {x, y};
 		return move;
 	}
 }
