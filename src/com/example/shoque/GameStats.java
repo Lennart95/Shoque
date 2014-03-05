970c08884770251c2eb28c4281357faef36a756c
Lennart van der Kamp <Lennartvanderkamp@gmail.com>
2014-03-05 15:20:53 +0100

    GameStats

---
+++ src/com/example/shoque/GameStats.java
@@ -0,0 +1,15 @@
+package com.example.shoque;
+
+public class GameStats {
+	int turn;
+	
+	/*
+	 * Deze methode houdt bij wie aan de beurt is. Er word gewerkt met een integer.
+	 */
+	public static int beurt(int turn) {
+		if (turn == 1){
+			
+		}
+		return turn;
+	}
+}
