2518e9b7cebef5fa8c2eae855fc2bb50950e6045
Wesley Oosterveen <wesleyoosterveen94@gmail.com>
2014-03-12 11:48:17 +0100

    formating

--- src/com/saxion/shoque/playground/model/Game.java
+++ src/com/saxion/shoque/playground/model/Game.java
@@ -1,31 +1,32 @@
 package com.saxion.shoque.playground.model;
 
 /**
- * Superclass for all games. 
-* 
+ * Superclass for all games.
+ * 
  * You should subclass this for your own game. In that subclass you will keep
  * track of all game-related state, like the score, who's turn it is, etc.
  * 
  * @author Paul de Groot
  */
 public abstract class Game {
-	
+
 	/**
 	 * Variables containing the 2 boards maintained by Game
 	 */
 	private SeashoqueBoard gameBoard;
 	private SeashoqueBoard enemyBoard;
-	
+
 	/**
 	 * Integer to keep track of current player
+	 * 1 is human player, 2 is AI
 	 */
 	private int turn = 1;
 	
 	/**
 	 * ??
 	 */
-	private int shots = 0; //what does this mean?
-	
+	private int shots = 0; // what does this mean?
+
 	/**
 	 * Should be true if the game is over, false if the game is still running.
 	 */
@@ -33,6 +34,7 @@ public abstract class Game {
 
 	/**
 	 * Called when you create a new game.
+	 * 
 	 * @param gameBoard
 	 */
 	public Game() {
@@ -42,104 +44,111 @@ public abstract class Game {
 		enemyBoard.setGame(this);
 	}
 
-	/** 
-	 * Returns the gameboard of the human player. 
+	/**
+	 * Returns the gameboard of the human player.
 	 */
 	public SeashoqueBoard getGameBoard() {
 		return gameBoard;
 	}
-	
-	/** 
-	 * Returns the gameboard of the AI. 
+
+	/**
+	 * Returns the gameboard of the AI.
 	 */
 	public SeashoqueBoard getEnemyBoard() {
 		return enemyBoard;
 	}
-	
+
 	/**
-	 * Handles stuff when next player should do it's thingey. 
+	 * Handles stuff when next player should do it's thingey.
+	 * 
 	 * @param t
 	 */
 	public void nextPlayer(int t) {
-		if (t == 1){
+		if (t == 1) {
 			turn = 2;
 		} else {
 			turn = 1;
 		}
-		//TODO: update some visual to alert the next player it's his turn
+		// TODO: update some visual to alert the next player it's his turn
 	}
 
-	//------------ ------------- ------------- ------------- -------------//
-	//------------ ------------- ------------- ------------- -------------//
-	//------------ ------------- ------------- ------------- -------------//
-	//----   TODO: some part where the game asks the user for ships  -----//
-	//------------ ------------- ------------- ------------- -------------//
-	//------------ ------------- ------------- ------------- -------------//
-	//------------ ------------- ------------- ------------- -------------//
-	
-	
+	// ------------ ------------- ------------- ------------- -------------//
+	// ------------ ------------- ------------- ------------- -------------//
+	// ------------ ------------- ------------- ------------- -------------//
+	// ---- TODO: some part where the game asks the user for ships -----//
+	// ------------ ------------- ------------- ------------- -------------//
+	// ------------ ------------- ------------- ------------- -------------//
+	// ------------ ------------- ------------- ------------- -------------//
+
 	/**
-	 * Return true if the all the fields of the given board have a gameObject. Return false if an empty field has been found.
+	 * Return true if the all the fields of the given board have a gameObject.
+	 * Return false if an empty field has been found.
+	 * 
 	 * @return
 	 */
 	public boolean isFull(SeashoqueBoard board) {
 		boolean result = true;
-		for (int x = 0; x<board.getDim(); x++){
-			for (int y = 0; y<board.getDim(); y++){
-				if (board.getObject(x, y)==null){
+		for (int x = 0; x < board.getDim(); x++) {
+			for (int y = 0; y < board.getDim(); y++) {
+				if (board.getObject(x, y) == null) {
 					result = false;
 				}
 			}
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Get the dimensions of the board
+	 * 
 	 * @return
 	 */
-	public int getDim(){
+	public int getDim() {
 		return gameBoard.getDim();
 	}
-	
+
 	/**
-	 * isFirstShot checks if the first shot has been fired. 
+	 * isFirstShot checks if the first shot has been fired.
+	 * 
 	 * @param shots
 	 */
-	public void isFirstShot(int shots){ //There are better solutions -casper
-		if (shots == 0){
+	public void isFirstShot(int shots) { // There are better solutions -casper
+		if (shots == 0) {
 			// TODO: ??
-		}	
+		}
 	}
-	
+
 	/**
 	 * Most important function: this is what the game consists of.
+	 * 
 	 * @param target
 	 * @param x
 	 * @param y
 	 */
-	public void shoot(SeashoqueBoard target, int x, int y){
-		//TODO: check if opposite of target is allowed to shoot yet
-		//TODO: check hit/miss
-		//TODO: update board
-		//TODO: on hit: ask for another move, on miss: call nextPlayer();
+	public void shoot(SeashoqueBoard target, int x, int y) {
+
+		// TODO: check if opposite of target is allowed to shoot yet
+		// TODO: check hit/miss
+		// TODO: update board
+		// TODO: on hit: ask for another move, on miss: call nextPlayer();
 	}
-	
+
 	/**
-	 * isGameOver is called to check whether all ships of one board are dead. Should be called after every shot.
+	 * isGameOver is called to check whether all ships of one board are dead.
+	 * Should be called after every shot.
 	 */
-	public boolean isGameOver(){
-		//TODO: check is there is a board with no alive gameObjects.
+	public boolean isGameOver() {
+		// TODO: check is there is a board with no alive gameObjects.
 		return false;
 	}
-	
+
 	/**
 	 * Run the game, keep turns, accept moves etc.
 	 */
-	public void run(){
-		while(!isFull(gameBoard) ||!isFull(enemyBoard) || !gameover){
-			//TODO: constant check for input (is this necessary in android?)
+	public void run() {
+		while (!isFull(gameBoard) || !isFull(enemyBoard) || !gameover) {
+			// TODO: constant check for input (is this necessary in android?)
 		}
 	}
-	
+
 }
