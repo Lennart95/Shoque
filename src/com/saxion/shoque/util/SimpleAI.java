d9db995e640705d73641784fdb04552cfb4f24c8
Cmitz <cmitzz@gmail.com>
2014-04-09 12:23:58 +0200

    New Boat Positions in dumb AI

--- src/com/saxion/shoque/util/SimpleAI.java
+++ src/com/saxion/shoque/util/SimpleAI.java
@@ -29,7 +29,7 @@ public class SimpleAI implements AI{
 	 * Minesweeper = 4
 	 */
 	public int[][] getShips(){
-		int[][] result = {{0,5,2,0},{1,4,3,1},{2,1,6,0},{3,5,0,1},{4,4,1,0}};
+		int[][] result = {{0,1,0,1},{1,8,1,1},{2,3,3,1},{3,4,9,1},{4,2,5,0}};
 		
 		
 		return result;
